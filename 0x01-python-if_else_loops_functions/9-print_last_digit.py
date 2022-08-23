a60fcd8f05041324d4c27658c83fe7d0ee0e7b61
bioaddict <rafgangbadja@gmail.com>
2022-08-24 00:10:29 +0100

    Prints the last digit of a number

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def print_last_digit(number):
+    """prints the last digit of a number"""
+    last_digit = number % 10
+    if (number < 0):
+        last_digit = 10 - last_digit
+    print("{}".format(last_digit), end="")
+    return (last_digit)
