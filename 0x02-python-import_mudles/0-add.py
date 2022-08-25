26b22440d51b0be5f72741df72343bad505b3fc9
bioaddict <rafgangbadja@gmail.com>
2022-08-25 17:08:17 +0000

    Imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

---
+++ 0x02-python-import_mudles/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-add.py
+# Albert G
+if __name__== "__main__":
+    """Print the sum of a and b."""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
