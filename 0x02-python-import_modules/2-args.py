79a896ade6dcb7ae7365e2e2e8701787c3ef293f
bioaddict <rafgangbadja@gmail.com>
2022-08-25 21:14:37 +0000

    Fix the formating error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,11 +8,11 @@ def print_args(argv):
         print("0 arguments.")
     elif (len(argv) == 2):
         print("{:d} argument:".format(len(argv) - 1))
-        print("1: {:d}".format(argv[1]))
+        print("1: {:s}".format(argv[1]))
     else:
         print("{:d} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
-            print("{:d}: {:d}".format(i, argv[i]))
+            print("{:d}: {:s}".format(i, argv[i]))
 
 if __name__ == "__main__":
     import sys
