e26536aecbad93e2654540faf16d75068f925433
bioaddict <rafgangbadja@gmail.com>
2022-08-25 22:37:13 +0000

    A  program that prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# 3-infinite_add.py
+# Albert G
+
+def infinite_addition(argv):
+    """ Computes the sum of all its arguments
+    
+    Args:
+        argv: list of command line arguments
+
+    Returns:
+        The return value: sum of all of its arguments
+    """
+    sum = 0
+
+    if (len(argv) <= 1):
+        pass
+    else:
+        for i in range(1, len(argv)):
+            sum = sum + int(argv[i])
+
+    return (sum)
+
+if __name__ == "__main__":
+    import sys
+    print("{:d}".format(infinite_addition(sys.argv)))
