52791e84f728554cfee5cb969f747bbb457056b4
bioaddict <rafgangbadja@gmail.com>
2022-08-28 16:36:38 +0100

    Fix the failed output

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    """Retrieve an element from a list."""
     if (idx < 0 or idx > (len(my_list) - 1)):
         return (None)
-    else:
-        for i in range(len(my_list) - 1):
-            if (i == idx):
-                return (my_list[i])
+    return (my_list[idx])
