9f82675a196cad5e20db19da9e3e677698f85401
bioaddict <rafgangbadja@gmail.com>
2022-08-30 01:14:34 +0100

    Fix the error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
 def print_reversed_list_integer(my_list=[]):
-    i = len(my_list) - 1
-    while (i >= 0):
-        print("{:d}".format(my_list[i]))
-        i = i - 1
+    """Prints all integers of a list in reverse order."""
+    if isinstance(my_list, list):
+        my_list.reverse()
+        i = 0
+        while (i <= (len(my_list) - 1)):
+            print("{:d}".format(my_list[i]))
+        i = i + 1
