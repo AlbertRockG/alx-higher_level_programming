12a1f0a6e442c01de204cf25ff648205d6f52f20
bioaddict <rafgangbadja@gmail.com>
2022-08-30 01:17:20 +0100

    Fix the infinite loop

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,6 +5,6 @@ def print_reversed_list_integer(my_list=[]):
     if isinstance(my_list, list):
         my_list.reverse()
         i = 0
-        while (i <= (len(my_list) - 1)):
+        while (i < len(my_list)):
             print("{:d}".format(my_list[i]))
-        i = i + 1
+            i = i + 1
