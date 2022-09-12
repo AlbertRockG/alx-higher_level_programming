c03ac9556fd5ed65b86fac0b16ace5e4c8c30224
bioaddict <rafgangbadja@gmail.com>
2022-09-12 16:38:35 +0100

    Add a function that raises a NameError with a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# AlbertG <rafgangbadja@gmail.com>
+
+def raise_exception_msg(message=""):
+    """Raises a NameError exception with a message"""
+    raise NameError(message)
