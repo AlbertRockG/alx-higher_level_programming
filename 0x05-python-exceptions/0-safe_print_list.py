3fddf2b1afff8f7eddad794a8cb67997f6532105
bioaddict <rafgangbadja@gmail.com>
2022-09-12 12:02:34 +0100

    Fix the pycodestyle error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -16,7 +16,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            elementsNumber+=1
+            elementsNumber += 1
         except IndexError:
             break
     print("")
