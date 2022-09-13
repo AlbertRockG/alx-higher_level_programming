4ea862089145c6cbba10f1840a66635e7901cbec
bioaddict <rafgangbadja@gmail.com>
2022-09-14 00:45:51 +0100

    Fix the pycodestyle error

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # 0-square.py
 # AlbertG
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """Define class named Square"""
+
+
 class Square:
     """An empty class that defines a square"""
+    pass
