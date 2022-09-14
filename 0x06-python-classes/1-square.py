e73ddc8ccd910d3618f47fcc47e8a5e35fd9c39f
bioaddict <rafgangbadja@gmail.com>
2022-09-14 01:06:48 +0100

    A class that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 1-square.py
+# AlbertG
+# -*- encoding: utf-8 -*-
+"""Define a class named Square"""
+
+
+class Square:
+    """Represents a square"""
+
+
+    def __init__(self,):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
