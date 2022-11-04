6a8619c7836970aedec5c086fab420ab3df67907
bioaddict <rafgangbadja@gmail.com>
2022-11-04 22:32:50 +0100

    Fix the typo

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- creates a table called `first_table` in the current database in my MySQL server
-CREATE TABLE IF NOT IXISTS `fist_table` (`id` INT, `name` VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `fist_table` (`id` INT, `name` VARCHAR(256));
