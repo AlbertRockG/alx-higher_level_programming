7e1f963c0df93c953a7c6e6a7e6c9f8857a6c8b9
bioaddict <rafgangbadja@gmail.com>
2022-12-04 22:02:35 +0100

    Fix check errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,4 +8,8 @@ if __name__ == "__main__":
             passwd=arg[2], database=arg[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    [print(state) for state in cursor.fetchall()]
+    db = cursor.fetchall()
+    for i in db:
+        print(i)
+    cursor.close()
+    db.close()
