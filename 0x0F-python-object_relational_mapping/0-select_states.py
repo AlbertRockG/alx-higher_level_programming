01a11753d1749b73b8765b670b128321df577fd2
bioaddict <rafgangbadja@gmail.com>
2022-12-04 21:46:41 +0100

    Fix error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# Usage: ./0-select_states.py <mysql username> \
-#                            <mysql password> \
-#                            <database name>
+
+
 import MySQLdb
 from sys import argv
 
@@ -10,7 +9,6 @@ lists all states with a name starting
 with N (upper N) from the database
 hbtn_0e_0_usa
 '''
-
 if __name__ == '__main__':
     db = MySQLdb.connect(
             host="localhost", port=3306, user=arg[1],
