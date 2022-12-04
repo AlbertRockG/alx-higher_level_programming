9a7837211f62655f28d7708d527bfba06b16df61
bioaddict <rafgangbadja@gmail.com>
2022-12-04 17:21:35 +0100

    Fix the error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,23 +2,19 @@
 # Usage: ./0-select_states.py <mysql username> \
 #                            <mysql password> \
 #                            <database name>
-import sys
 import MySQLdb
+from sys import argv
+
 '''
 lists all states with a name starting
 with N (upper N) from the database
 hbtn_0e_0_usa
 '''
+
 if __name__ == '__main__':
     db = MySQLdb.connect(
             host="localhost", port=3306, user=arg[1],
             passwd=arg[2], database=arg[3])
     cursor = db.cursor()
-    cursor.execute(
-            "SELECT * FROM states ORDER BY id ASC"
-            )
-    db = cursor.fetchall()
-    for i in db:
-        print(i)
-    cursor.close()
-    db.close()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    [print(state) for state in cursor.fetchall()]
