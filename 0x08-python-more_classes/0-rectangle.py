ae69ed4f91cd72c9085022d648d87ea2ad03ae33
bioaddict <rafgangbadja@gmail.com>
2023-01-01 15:25:10 +0100

    An empty class Reactangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Albertg
+# 0-rectangle.py
+# -*- coding: utf-8 -*-
+"""Defines a class named Rectangle"""
+
+
+class Rectangle():
+    """Reprensents an empty class that defines a Rectangle"""
+    pass
