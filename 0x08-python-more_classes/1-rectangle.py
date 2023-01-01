15b28976c4541f0ad8847f386ac4c3777e95d708
bioaddict <rafgangbadja@gmail.com>
2023-01-01 15:52:21 +0100

    Defines a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# Albertg
+# 0-rectangle.py
+# -*- coding: utf-8 -*-
+"""Defines a class named Rectangle"""
+
+
+class Rectangle():
+    """Reprensents a Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle object.
+
+        Args:
+            width (int): The width of the Rectangle.
+            height (int): The height of the Rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
