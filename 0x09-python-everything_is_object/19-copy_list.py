ec602401ba85abbcb6f7fb23d24760cf212a7955
bioaddict <rafgangbadja@gmail.com>
2023-01-04 00:55:45 +0100

    Shallow copy of list

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,17 +1,3 @@
 #!/usr/bin/python3
-# 19-copy_list.py
-# Albertg
-# -*- coding: utf-8 -*-
-
-"""A module with a function to copy a list"""
-
-
-
 def copy_list(l):
-    """Copy a list.
-
-    Args:
-        l (list): The given list to copy.
-    """
-    l1 = l.copy()
-    return l1
+   return l.copy() 
