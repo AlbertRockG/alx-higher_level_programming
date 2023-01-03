8a327a6fdecd0c5c397c3467a617fca13c70a737
bioaddict <rafgangbadja@gmail.com>
2023-01-03 11:15:40 +0100

    Test files

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -97,5 +97,4 @@ class Rectangle():
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-        else:
-            return rect_2
+        return rect_2
