159ac9e3066c5b268e4b57d437fa6781ea5f8a69
bioaddict <rafgangbadja@gmail.com>
2023-07-04 01:40:39 +0100

    Fix typo

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,9 +25,9 @@ class Rectangle():
 
     @width.setter
     def width(self, value):
-        if not instance(width):
+        if not instance(value, int):
             raise TypeError("width must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -38,8 +38,8 @@ class Rectangle():
 
     @height.setter
     def height(self, value):
-        if not instance(height):
+        if not instance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
