4d0f348d3123d6b55ce730774f19e53442f22e75
bioaddict <rafgangbadja@gmail.com>
2023-07-04 15:26:48 +0100

    Fix bug

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -68,8 +68,7 @@ class Rectangle():
             return ""
         rect = []
         for i in range(self.__height):
-            [rect.append(str(Rectangle.print_symbol))
-             for j in range(self.__width)]
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append('\n')
         return "".join(rect)
