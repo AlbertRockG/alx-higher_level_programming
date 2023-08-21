6558a71a4ce5688eb280c6207930303f10c84d94
bioaddict <rafgangbadja@gmail.com>
2023-08-21 01:21:20 +0100

    Using SUM and LEFT JOIN

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,11 @@
+-- Lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_shows.title - rating sum
+-- Results must be sorted in descending order by the rating
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT ts.`title`, SUM(`rate`) as `rating`
+    FROM `tv_shows` AS ts
+        LEFT JOIN `tv_show_ratings` AS tsr
+        ON ts.`id` = tsr.`show_id`
+    GROUP BY ts.`title`
+    ORDER BY `rating` DESC;
