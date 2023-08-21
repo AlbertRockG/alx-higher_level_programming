0ed2f8b2e952cbc422893e3b1d4131533f6247a6
bioaddict <rafgangbadja@gmail.com>
2023-08-21 01:36:58 +0100

    Fix SQL styling

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,7 +3,7 @@
 -- Results must be sorted in descending order by the rating
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT ts.`title`, SUM(`rate`) as `rating`
+SELECT ts.`title`, SUM(`rate`) AS `rating`
     FROM `tv_shows` AS ts
         LEFT JOIN `tv_show_ratings` AS tsr
         ON ts.`id` = tsr.`show_id`
