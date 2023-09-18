31a414beb5aa7e56bc3423bc03731dced4de9aa6
bioaddict <rafgangbadja@gmail.com>
2023-09-18 05:06:48 +0300

    Fix typo and push scripts of the remaining tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
 """ This module lists all the states from the
     database hbtn_0e_0_usa.
 """
 
-import sys
 import MySQLdb
+import sys
 
 
 def main():
@@ -18,20 +14,18 @@ def main():
     """
 
     # Create a database connection
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=sys.argv[1],
-        passwd=sys.argv[2], db=sys.argv[3], charset="uf8mb4"
-    )
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM `states` ORDER BY id ASC"
-    )
-
-    for row in cur.fetchall():
+    conn = MySQLdb.connect(
+                host="localhost", port=3306, user=sys.argv[1],
+                passwd=sys.argv[2], db=sys.argv[3], charset="utf8mb4"
+            )
+    cur = conn.cursor()
+    # Select states
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-
     cur.close()
-    db.close()
+    conn.close()
 
 
 if __name__ == "__main__":
