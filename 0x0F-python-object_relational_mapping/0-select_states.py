e193b1f19c59631846979b56a27b45a2dd383bca
bioaddict <rafgangbadja@gmail.com>
2023-09-18 04:32:02 +0300

    Fix the bug to meet the requirements

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,12 +3,35 @@
 # Usage: ./0-select_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
+""" This module lists all the states from the
+    database hbtn_0e_0_usa.
+"""
 
 import sys
 import MySQLdb
 
+
+def main():
+    """
+        Function containing code to select all the states
+        from the database.
+    """
+
+    # Create a database connection
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3], charset="uf8mb4"
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM `states` ORDER BY id ASC"
+    )
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    main()
