ea9e19dadeddd51a9c06d69e15cdfa583ee4a037
bioaddict <rafgangbadja@gmail.com>
2023-10-01 21:15:09 +0100

    Fix typo to meet requirements

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL
-# and displays the size of the body of the response
+# Takes in a URL, sends a request to that URL and displays the size of the body of the response
 curl -s "$1" | wc -c
