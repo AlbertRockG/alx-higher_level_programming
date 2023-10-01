399ffdb1e16f3b85a60b0d265af008520fe101ba
bioaddict <rafgangbadja@gmail.com>
2023-10-01 22:05:54 +0100

    Fix typo in awk synthax

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a given URL and displays the size of the body of the response
-curl -sI "$1" | awk -v IGNORECASE=1 '/^Content-Length/ { print $2}'
+curl -sI "$1" | awk -v IGNORECASE=1 '/^Content-Length/ { print $2 }'
