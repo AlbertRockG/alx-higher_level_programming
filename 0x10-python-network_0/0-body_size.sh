c3c1f5c953713562f02274208775209fa35f3cd0
bioaddict <rafgangbadja@gmail.com>
2023-10-01 22:00:14 +0100

    Use awk to parse curl -sI output

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL and displays the size of the body of the response
-curl -s "$1" | wc -c
+# Sends a request to a given URL and displays the size of the body of the response
+curl -sI "$1" | awk -v IGNORECASE=1 '/^Content-Length/ { print $2}'
